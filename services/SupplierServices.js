80ffc0751b1b4ea4e8ddcf485c87186a7b513cf3
gsaiteja001 <saiteja244139@gmail.com>
2024-12-02 18:47:35 +0530

    Create SupplierServices.js

---
+++ services/SupplierServices.js
@@ -0,0 +1,30 @@
+const Supplier = require('./models/Supplier');
+
+async function addProductToSupplier(supplierId, productId, suppliedQuantity, leadTime) {
+  try {
+    const supplier = await Supplier.findById(supplierId);
+    if (!supplier) {
+      throw new Error('Supplier not found.');
+    }
+
+    await supplier.addProduct(productId, suppliedQuantity, leadTime);
+    console.log('Product added/updated successfully.');
+  } catch (error) {
+    console.error('Error adding product to supplier:', error.message);
+  }
+}
+async function removeProductFromSupplier(supplierId, productId) {
+  try {
+    const supplier = await Supplier.findById(supplierId);
+    if (!supplier) {
+      throw new Error('Supplier not found.');
+    }
+
+    await supplier.removeProduct(productId);
+    console.log('Product removed successfully.');
+  } catch (error) {
+    console.error('Error removing product from supplier:', error.message);
+  }
+}
+
+
