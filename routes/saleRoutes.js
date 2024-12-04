f6f369aee2fa75fd09798c587b9117935e659714
gsaiteja001 <saiteja244139@gmail.com>
2024-12-04 17:59:43 +0530

    Update saleRoutes.js

--- routes/saleRoutes.js
+++ routes/saleRoutes.js
@@ -3,9 +3,14 @@
 const express = require('express');
 const { handleRecordSale } = require('../controllers/saleController');
 
+const purchaseController = require('../controllers/purchaseController');
+
 const router = express.Router();
 
 // POST /api/sales/record
 router.post('/record', handleRecordSale);
+
+// Route for creating a purchase
+router.post('/purchase-record/create', purchaseController.createPurchase);
 
 module.exports = router;
