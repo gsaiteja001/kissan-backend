826a9a888df947517b481e0f3bbfe80f75e1a33a
gsaiteja001 <saiteja244139@gmail.com>
2024-12-04 12:06:50 +0530

    Update Purchase.js

--- modal/Purchase.js
+++ modal/Purchase.js
@@ -1,3 +1,5 @@
+// models/Purchase.js
+
 const mongoose = require('mongoose');
 const { v4: uuidv4 } = require('uuid');
 
@@ -39,7 +41,7 @@ const PurchaseSchema = new mongoose.Schema({
     amountPaid: { type: Number, min: [0, 'Amount paid cannot be negative'] },
   },
   notes: { type: String },
-  stockTransaction: {
+  stockTransaction: { // Reference to StockTransaction
     type: mongoose.Schema.Types.ObjectId,
     ref: 'StockTransaction',
     required: false,
