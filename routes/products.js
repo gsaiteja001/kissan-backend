e71718d4d9357a0971f858aca7fc51f704892f78
gsaiteja001 <saiteja244139@gmail.com>
2024-12-06 10:32:29 +0530

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const translateProduct = require('../utils/translateProduct');
 const Product = require('../modal/product'); 
-const { getProductIdsFromWarehouses } = require('../controllers/nearBywarehouse');
+const { getProductIdsFromWarehouses } = require('../services/inventoryService');
 
 
 // Similar Products Route
