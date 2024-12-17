0a576bb7c8d1d8f959d93d931cfb04a25d29f018
gsaiteja001 <saiteja244139@gmail.com>
2024-12-17 15:08:35 +0530

    Update saleRoutes.js

--- routes/saleRoutes.js
+++ routes/saleRoutes.js
@@ -13,4 +13,7 @@ router.post('/record', handleRecordSale);
 // Route for creating a purchase
 router.post('/purchase-record/create', purchaseController.createPurchase);
 
+// New Route: Fetch purchases by warehouseId
+router.get('/warehouse/:warehouseId', purchaseController.getPurchasesByWarehouseId);
+
 module.exports = router;
